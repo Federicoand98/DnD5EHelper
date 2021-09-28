1e683951b65f1916759ca8c2ab49c80ed7ca8c80
Federico Andrucci <federico.andrucci@gmail.com>
2021-09-28 16:20:40 +0200

    fix

--- index.js
+++ index.js
@@ -23,48 +23,47 @@ var res;
 client.login(process.env.TOKEN);
 
 client.once('ready', () => {
-  const xml = fs.readFileSync('core.xml');
-  const parser = new xml2js.Parser();
+	const xml = fs.readFileSync('core.xml');
+	const parser = new xml2js.Parser();
 
-  xml2js.parseString(xml, {mergeAttrs: true}, (err, result) => {
-      if(err) {
-        console.log(err);
-        throw err;
-      }
+	xml2js.parseString(xml, {mergeAttrs: true}, (err, result) => {
+		if(err) {
+			console.log(err);
+			throw err;
+		}
 
-      res = JSON.stringify(result, null , 4);
-      json = JSON.parse(res);
+		res = JSON.stringify(result, null , 4);
+		json = JSON.parse(res);
     });
 
-  console.log("Online");
+	console.log("Online");
 });
 
 client.on('message', message => {
-  var parts = message.content.split(" ");
-  var search = parts.slice(1).join(" ");
-  var field = "name";
-  var embed;
-  
-  if(parts[0] === "!item") {
+	var parts = message.content.split(" ");
+	var search = parts.slice(1).join(" ");
+	var field = "name";
+	var embed;
 
-    commands.searchIntoJson(1, field, search, message, json, Client);
-  
-  } else if(parts[0] === "!spell") {
+	if(parts[0] === "!item") {
 
-    commands.searchIntoJson(2, field, search, message, json, Client);
+		commands.searchIntoJson(1, field, search, message, json, Client);
 
-  } else if(parts[0] == "!monster") {
+	} else if(parts[0] === "!spell") {
 
-    commands.searchIntoJson(3, field, search, message, json, Client);
+		commands.searchIntoJson(2, field, search, message, json, Client);
 
-  } else if(message.content.startsWith('!d')) {
+	} else if(parts[0] == "!monster") {
 
-    commands.roll(message, Client);
+		commands.searchIntoJson(3, field, search, message, json, Client);
+
+	} else if(message.content.startsWith('!d')) {
+
+		commands.roll(message, Client);
     
-  } else if(message.content.indexOf('d') == 2) {
+	} else if(message.content.indexOf('d') == 2) {
     
-    commands.m_roll(message, Client);
+		commands.m_roll(message, Client);
 
-  }
-
+	}
 });
